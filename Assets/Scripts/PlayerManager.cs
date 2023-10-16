e5a5bfe30499d9ef6348194efbfabd89968ec392
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-10-16 10:09:26 +0530

    PlayerManager Script added

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+
+    #region MonoBehaviour Callbacks
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Debug.Log("Instantiated Player Controller");
+        //Instantiate our player controller
+    }
+
+    #endregion
+}
