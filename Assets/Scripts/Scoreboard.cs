a35c64420af409dbdd381248ad6bd861a62f3fe0
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-10-19 09:52:29 +0530

    ScoreBoard Display part-1 Done

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,43 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Transform container;
+    [SerializeField] GameObject scoreboardItemPrefab;
+
+    Dictionary<Player, ScoreboardItem> scoreboardItems = new Dictionary<Player, ScoreboardItem>();
+
+    private void Start()
+    {
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            AddScoreboardItem(player);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        AddScoreboardItem(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RemoveScoreboardItem(otherPlayer);
+    }
+
+    void AddScoreboardItem(Player player)
+    {
+        ScoreboardItem item = Instantiate(scoreboardItemPrefab,container).GetComponent<ScoreboardItem>();
+        item.Initialize(player);
+    }
+
+    void RemoveScoreboardItem(Player player )
+    {
+        Destroy(scoreboardItems[player].gameObject);
+        scoreboardItems.Remove(player);
+    }
+}
